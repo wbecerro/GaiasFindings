e24f0fc34190e8252fba11ab3826aaa00dae77a6
wbe <34304532+wbecerro@users.noreply.github.com>
2026-05-08 20:33:24 +0200

    Añadidos objetos.

--- src/main/java/wbe/gaiasFindings/items/RuneItem.java
+++ src/main/java/wbe/gaiasFindings/items/RuneItem.java
@@ -1,4 +1,46 @@
 package wbe.gaiasFindings.items;
 
-public class RuneItem {
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+import wbe.gaiasFindings.GaiasFindings;
+import wbe.gaiasFindings.config.Rune;
+
+import java.util.ArrayList;
+
+public class RuneItem extends ItemStack {
+
+    public RuneItem(Rune rune) {
+        super(rune.getMaterial());
+
+        ItemMeta meta;
+        if(hasItemMeta()) {
+            meta = getItemMeta();
+        } else {
+            meta = Bukkit.getItemFactory().getItemMeta(rune.getMaterial());
+        }
+
+        meta.setDisplayName(GaiasFindings.config.runeName.replace("%rune_name%", rune.getName()));
+
+        ArrayList<String> lore = new ArrayList<>();
+        String enchant = rune.getEnchantment().getKey().getKey().toLowerCase().replace("_", " ");
+        for(String line : GaiasFindings.config.runeLore) {
+            lore.add(line.replace("&", "§").replace("%enchant%", enchant));
+        }
+
+        meta.setLore(lore);
+        NamespacedKey typeKey = new NamespacedKey(GaiasFindings.getInstance(), "runeType");
+        meta.getPersistentDataContainer().set(typeKey, PersistentDataType.STRING, rune.getId());
+
+        if(GaiasFindings.config.runeGlow) {
+            meta.addEnchant(Enchantment.UNBREAKING, 1, true);
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        }
+
+        setItemMeta(meta);
+    }
 }
--- src/main/java/wbe/gaiasFindings/items/Shovel.java
+++ src/main/java/wbe/gaiasFindings/items/Shovel.java
@@ -1,4 +1,43 @@
 package wbe.gaiasFindings.items;
 
-public class Shovel {
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+import wbe.gaiasFindings.GaiasFindings;
+
+import java.util.ArrayList;
+
+public class Shovel extends ItemStack {
+
+    public Shovel(Material material, double runeChance) {
+        super(material);
+
+        ItemMeta meta;
+        if(hasItemMeta()) {
+            meta = getItemMeta();
+        } else {
+            meta = Bukkit.getItemFactory().getItemMeta(material);
+        }
+
+        meta.setDisplayName(GaiasFindings.config.shovelName);
+
+        ArrayList<String> lore = new ArrayList<>();
+        for(String line : GaiasFindings.config.shovelLore) {
+            lore.add(line.replace("&", "§"));
+        }
+
+        lore.add(GaiasFindings.config.shovelRuneChance.replace("%rune_chance%", String.valueOf(runeChance)));
+        meta.setLore(lore);
+
+        NamespacedKey runeKey = new NamespacedKey(GaiasFindings.getInstance(), "runeChance");
+        meta.getPersistentDataContainer().set(runeKey, PersistentDataType.DOUBLE, runeChance);
+
+        NamespacedKey fortuneKey = new NamespacedKey(GaiasFindings.getInstance(), "fortuneLevel");
+        meta.getPersistentDataContainer().set(fortuneKey, PersistentDataType.INTEGER, 0);
+
+        setItemMeta(meta);
+    }
 }
