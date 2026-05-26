1defb43e5853ee4d47ea1dd1d021c9859133ab6f
wbe <34304532+wbecerro@users.noreply.github.com>
2026-05-26 12:19:23 +0200

    Cambiada la aplicación de las runas a solo clic izquierdo.

--- src/main/java/wbe/gaiasFindings/listeners/InventoryClickListeners.java
+++ src/main/java/wbe/gaiasFindings/listeners/InventoryClickListeners.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
@@ -20,6 +21,10 @@ public class InventoryClickListeners implements Listener {
     public void applyGem(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
         if(!event.getAction().equals(InventoryAction.SWAP_WITH_CURSOR)) {
+            return;
+        }
+
+        if(!(event.getClick().equals(ClickType.LEFT))) {
             return;
         }
 
